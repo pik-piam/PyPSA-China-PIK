527f6dc0bd6051ffbba32a2a2e98cdde9f122682
irr-github <ivanrafaelramirez@gmail.com>
2024-10-24 14:48:30 +0200

    rename pop, fmt w black

--- workflow/scripts/build_heat_demand_profiles.py
+++ workflow/scripts/build_heat_demand_profiles.py
@@ -1,3 +1,4 @@
+# TODO: is this actually used?
 import logging
 from _helpers import configure_logging
 
@@ -8,11 +9,11 @@ import scipy as sp
 
 logger = logging.getLogger(__name__)
 
+
 def build_heat_demand_profiles():
 
-    with pd.HDFStore(snakemake.input.population_map, mode='r') as store:
-        pop_map = store['population_gridcell_map']
-
+    with pd.HDFStore(snakemake.input.population_map, mode="r") as store:
+        pop_map = store["population_gridcell_map"]
 
     cutout = atlite.Cutout(snakemake.input.cutout)
 
@@ -20,25 +21,22 @@ def build_heat_demand_profiles():
     index = pop_map.columns
     index.name = "provinces"
 
-    hd = cutout.heat_demand(matrix=pop_matrix,
-                            index=index,
-                            threshold=15.,
-                            a=1.,
-                            constant=0.,
-                            hour_shift=8.)
-    
+    hd = cutout.heat_demand(
+        matrix=pop_matrix, index=index, threshold=15.0, a=1.0, constant=0.0, hour_shift=8.0
+    )
+
     Hd_2020 = hd.to_pandas().divide(pop_map.sum())
-    Hd_2020.loc['2020-04-01':'2020-09-30'] = 0
-    
+    Hd_2020.loc["2020-04-01":"2020-09-30"] = 0
 
-    with pd.HDFStore(snakemake.output.daily_heat_demand, mode='w', complevel=4) as store:
-        store['heat_demand_profiles'] = Hd_2020
+    with pd.HDFStore(snakemake.output.daily_heat_demand, mode="w", complevel=4) as store:
+        store["heat_demand_profiles"] = Hd_2020
 
 
-if __name__ == '__main__':
-    if 'snakemake' not in globals():
+if __name__ == "__main__":
+    if "snakemake" not in globals():
         from _helpers import mock_snakemake
-        snakemake = mock_snakemake('build_heat_demand_profiles')
+
+        snakemake = mock_snakemake("build_heat_demand_profiles")
     configure_logging(snakemake)
 
     df = build_heat_demand_profiles()
--- workflow/scripts/constants.py
+++ workflow/scripts/constants.py
@@ -3,14 +3,55 @@ import numpy as np
 CRS = 4326  # WGS84
 YEARBOOK_DATA2POP = 1e4
 POP_YEAR = 2020
+FREQ = "1H"
 
-PROV_NAMES = ['Anhui', 'Beijing', 'Chongqing', 'Fujian', 'Gansu', 'Guangdong',
-       'Guangxi', 'Guizhou', 'Hainan', 'Hebei', 'Heilongjiang', 'Henan',
-       'Hubei', 'Hunan','InnerMongolia', 'Jiangsu', 'Jiangxi', 'Jilin', 'Liaoning',
-       'Ningxia', 'Qinghai', 'Shaanxi', 'Shandong',
-       'Shanghai', 'Shanxi', 'Sichuan', 'Tianjin', 'Tibet', 'Xinjiang',
-       'Yunnan', 'Zhejiang']
+PROV_NAMES = [
+    "Anhui",
+    "Beijing",
+    "Chongqing",
+    "Fujian",
+    "Gansu",
+    "Guangdong",
+    "Guangxi",
+    "Guizhou",
+    "Hainan",
+    "Hebei",
+    "Heilongjiang",
+    "Henan",
+    "Hubei",
+    "Hunan",
+    "InnerMongolia",
+    "Jiangsu",
+    "Jiangxi",
+    "Jilin",
+    "Liaoning",
+    "Ningxia",
+    "Qinghai",
+    "Shaanxi",
+    "Shandong",
+    "Shanghai",
+    "Shanxi",
+    "Sichuan",
+    "Tianjin",
+    "Tibet",
+    "Xinjiang",
+    "Yunnan",
+    "Zhejiang",
+]
 
-OFFSHORE_WIND_NODES = np.array(['Fujian', 'Guangdong', 'Guangxi', 'Hainan', 'Hebei', 'Jiangsu',
-       'Liaoning', 'Shandong', 'Shanghai', 'Tianjin', 'Zhejiang'],
-      dtype=str)
+OFFSHORE_WIND_NODES = np.array(
+    [
+        "Fujian",
+        "Guangdong",
+        "Guangxi",
+        "Hainan",
+        "Hebei",
+        "Jiangsu",
+        "Liaoning",
+        "Shandong",
+        "Shanghai",
+        "Tianjin",
+        "Zhejiang",
+    ],
+    dtype=str,
+)
