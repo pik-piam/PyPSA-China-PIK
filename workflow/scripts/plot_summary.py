b0981f10ce1ed3db2ad083a7cff7cf8f710ff130
irr-github <ivan.ramirez@pik-potsdam.de>
2024-11-14 18:18:45 +0100

    fix style (make legal)

--- workflow/scripts/plot_summary.py
+++ workflow/scripts/plot_summary.py
@@ -4,30 +4,19 @@
 
 """
 Plots energy and cost summaries for solved networks.
-
-Relevant Settings
------------------
-
-Inputs
-------
-
-Outputs
--------
-
-Description
------------
-
 """
 
 import os
 import logging
-from _helpers import configure_logging
+from _helpers import configure_logging, mock_snakemake
 
 import pandas as pd
 import matplotlib.pyplot as plt
+
 plt.style.use("ggplot")
 
 logger = logging.getLogger(__name__)
+
 
 # consolidate and rename
 def rename_techs(label):
@@ -36,16 +25,9 @@ def rename_techs(label):
         "decentral ",
     ]
 
-    rename_if_contains_dict = {
-        "water tanks": "hot water storage",
-        "H2": "H2",
-        "coal cc": "CC"
-    }
+    rename_if_contains_dict = {"water tanks": "hot water storage", "H2": "H2", "coal cc": "CC"}
 
-    rename_if_contains = [
-        "gas",
-        "coal"
-    ]
+    rename_if_contains = ["gas", "coal"]
 
     rename = {
         "solar": "solar PV",
@@ -60,7 +42,7 @@ def rename_techs(label):
         "AC": "transmission lines",
         "CO2 capture": "biomass carbon capture",
         "CC": "coal carbon capture",
-        "battery": "battery"
+        "battery": "battery",
     }
 
     for ptr in prefix_to_remove:
@@ -116,151 +98,130 @@ preferred_order = pd.Index(
 )
 
 
+def plot_costs(results_file: os.PathLike, config: dict, fig_name: os.PathLike = None):
+    """plot the costs
 
-def plot_costs(infn, config, fn=None):
+    Args:
+        results_file (os.PathLike): the input csv
+        config (dict): the configuration for plotting
+        fig_name (os.PathLike, optional): the figure name. Defaults to None.
+    """
+    # For now ignore the simpl header
 
-    ## For now ignore the simpl header
-    cost_df = pd.read_csv(infn,index_col=list(range(3)),header=[1])
-
+    cost_df = pd.read_csv(results_file, index_col=list(range(3)), header=[1])
     df = cost_df.groupby(cost_df.index.get_level_values(2)).sum()
 
-    #convert to billions
-    df = df/1e9
-
+    # TODO centralise ?
+    # convert to billions
+    df = df / 1e9
     df = df.groupby(df.index.map(rename_techs)).sum()
-
-    to_drop = df.index[df.max(axis=1) < config['plotting']['costs_plots_threshold']]
-
-    print("dropping")
-
-    print(df.loc[to_drop])
-
+    to_drop = df.index[df.max(axis=1) < config["plotting"]["costs_plots_threshold"]]
     df = df.drop(to_drop)
 
-    print(df.sum())
-
-    new_index = preferred_order.intersection(df.index).append(
-        df.index.difference(preferred_order)
-    )
+    new_index = preferred_order.intersection(df.index).append(df.index.difference(preferred_order))
 
     new_columns = df.sum().sort_values().index
 
     fig, ax = plt.subplots()
-    fig.set_size_inches((12,8))
+    fig.set_size_inches((12, 8))
 
-    df.loc[new_index,new_columns].T.plot(
+    df.loc[new_index, new_columns].T.plot(
         kind="bar",
         ax=ax,
         stacked=True,
-        color=[config['plotting']['tech_colors'][i] for i in new_index],
+        color=[config["plotting"]["tech_colors"][i] for i in new_index],
     )
 
-
-    handles,labels = ax.get_legend_handles_labels()
+    handles, labels = ax.get_legend_handles_labels()
 
     handles.reverse()
     labels.reverse()
 
-    ax.set_ylim([0,config['plotting']['costs_max']])
-
+    ax.set_ylim([0, config["plotting"]["costs_max"]])
     ax.set_ylabel("System Cost [EUR billion per year]")
-
     ax.set_xlabel("")
-
     ax.grid(axis="y")
-
-    ax.legend(handles,labels,ncol=4,bbox_to_anchor=[1, 1],loc="upper left")
-
+    ax.legend(handles, labels, ncol=4, bbox_to_anchor=[1, 1], loc="upper left")
 
     fig.tight_layout()
 
-    if fn is not None:
-        fig.savefig(fn, transparent=True)
+    if fig_name is not None:
+        fig.savefig(fig_name, transparent=True)
 
 
-def plot_energy(infn, config, fn=None):
+def plot_energy(results_file: os.PathLike, config: dict, fig_name=None):
+    """plot the costs
 
-    energy_df = pd.read_csv(infn, index_col=list(range(2)),header=[1])
-
+    Args:
+        results_file (os.PathLike): the input csv
+        config (dict): the configuration for plotting
+        fig_name (os.PathLike, optional): the figure name. Defaults to None.
+    """
+    energy_df = pd.read_csv(results_file, index_col=list(range(2)), header=[1])
     df = energy_df.groupby(energy_df.index.get_level_values(1)).sum()
-
-    #convert MWh to TWh
-    df = df/1e6
-
+    # convert MWh to TWh
+    df = df / 1e6
     df = df.groupby(df.index.map(rename_techs)).sum()
 
-    to_drop = df.index[df.abs().max(axis=1) < config['plotting']['energy_threshold']]
-
+    to_drop = df.index[df.abs().max(axis=1) < config["plotting"]["energy_threshold"]]
     logger.info(
         f"Dropping all technology with energy consumption or production below {config['plotting']['energy_threshold']} TWh/a"
     )
     logger.debug(df.loc[to_drop])
-
     df = df.drop(to_drop)
-
     logger.info(f"Total energy of {round(df.sum()[0])} TWh/a")
-
-    new_index = preferred_order.intersection(df.index).append(
-        df.index.difference(preferred_order)
-    )
-
+    new_index = preferred_order.intersection(df.index).append(df.index.difference(preferred_order))
     new_columns = df.columns.sort_values()
 
     fig, ax = plt.subplots()
-    fig.set_size_inches((12,8))
+    fig.set_size_inches((12, 8))
 
     logger.debug(df.loc[new_index, new_columns])
 
-    df.loc[new_index,new_columns].T.plot(
+    df.loc[new_index, new_columns].T.plot(
         kind="bar",
         ax=ax,
         stacked=True,
-        color=[config['plotting']['tech_colors'][i] for i in new_index],
+        color=[config["plotting"]["tech_colors"][i] for i in new_index],
     )
 
-    handles,labels = ax.get_legend_handles_labels()
+    handles, labels = ax.get_legend_handles_labels()
 
     handles.reverse()
     labels.reverse()
 
-    ax.set_ylim([config['plotting']['energy_min'], config['plotting']['energy_max']])
-
+    ax.set_ylim([config["plotting"]["energy_min"], config["plotting"]["energy_max"]])
     ax.set_ylabel("Energy [TWh/a]")
-
     ax.set_xlabel("")
-
     ax.grid(axis="y")
-
-    ax.legend(handles,labels,ncol=4,bbox_to_anchor=[1, 1],loc="upper left")
-
+    ax.legend(handles, labels, ncol=4, bbox_to_anchor=[1, 1], loc="upper left")
     fig.tight_layout()
 
-    if fn is not None:
-        fig.savefig(fn, transparent=True)
+    if fig_name is not None:
+        fig.savefig(fig_name, transparent=True)
 
 
 if __name__ == "__main__":
-    if 'snakemake' not in globals():
-        from _helpers import mock_snakemake
-        snakemake = mock_snakemake('plot_summary',
-                                   opts='ll',
-                                   topology ='current+Neighbor',
-                                   pathway ='exponential175',
-                                   planning_horizons="2020")
+    if "snakemake" not in globals():
+
+        snakemake = mock_snakemake(
+            "plot_summary",
+            opts="ll",
+            topology="current+Neighbor",
+            pathway="exponential175",
+            planning_horizons="2020",
+        )
     configure_logging(snakemake)
 
     config = snakemake.config
     wildcards = snakemake.wildcards
     logs = snakemake.log
-    out = snakemake.output
+    output_paths = snakemake.output
     paths = snakemake.input
 
-    Summary = ['energy', 'costs']
-    summary_i = 0
-    for summary in Summary:
-        try:
-            func = globals()[f"plot_{summary}"]
-        except KeyError:
-            raise RuntimeError(f"plotting function for {summary} has not been defined")
-        func(os.path.join(paths[0], f"{summary}.csv"), config, out[summary_i])
-        summary_i = summary_i + 1
+    data_paths = {
+        "energy": os.path.join(paths[0], "energy.csv"),
+        "costs": os.path.join(paths[0], "costs.csv"),
+    }
+    plot_costs(data_paths, config, output_paths.costs)
+    plot_energy(data_paths, config, output_paths.energy)
