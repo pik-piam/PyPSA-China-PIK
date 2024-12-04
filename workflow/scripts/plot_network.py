3f319c03e6ec3b393cdf3d7e3eda4b7ecd00841e
irr-github <ivanrafaelramirez@gmail.com>
2024-12-04 12:11:17 +0100

    break-up cost plot function, fix hardcoded

--- workflow/scripts/plot_network.py
+++ workflow/scripts/plot_network.py
@@ -9,274 +9,297 @@ import pandas as pd
 from pypsa.plot import add_legend_circles, add_legend_lines, add_legend_patches
 from plot_summary import preferred_order, rename_techs
 from _plot_utilities import assign_location, set_plot_style
-from _helpers import configure_logging, mock_snakemake, load_plot_style
+from _helpers import configure_logging, mock_snakemake, annualise_component_capex
+from constants import PLOT_COST_UNITS
+
 
 logger = logging.getLogger(__name__)
 
 
-def get_costs(costs: pd.DataFrame, tech_colors: dict):
-    """_summary_
+def make_cost_pies(cost_df: pd.DataFrame, tech_colors: dict) -> pd.DataFrame:
+    """Make pies for plotting
 
     Args:
-        costs (_type_): _description_
+        cost_df (pd.DataFrame): the costs
+        tech_colors (dict): the tech color config
 
     Returns:
-        _type_: _description_
+        pd.DataFrame: the pies per bus location
     """
-    costs = costs.groupby(costs.columns, axis=1).sum()
+
+    costs = cost_df.T.groupby(cost_df.columns).sum().T
+    # drop empty columns
     costs.drop(list(costs.columns[(costs == 0.0).all()]), axis=1, inplace=True)
-    new_columns = preferred_order.intersection(costs.columns).append(
+    reordered = preferred_order.intersection(costs.columns).append(
         costs.columns.difference(preferred_order)
     )
-    costs = costs[new_columns]
-    for item in new_columns:
-        if item not in tech_colors:
-            logger.warning(f"{item} not in config/plotting/tech_colors")
+    costs = costs[reordered]
+    # deal with missing colors
+    missing_colors = costs.columns.difference(tech_colors)
+    if len(missing_colors) > 0:
+        logger.warning(f"Missing colors in plot config for {missing_colors}")
+    dict(zip(missing_colors, [tech_colors.get("other", "pink")] * len(missing_colors)))
     costs = costs.stack()  # .sort_index()
     to_drop = costs.index.levels[0].symmetric_difference(n.buses.index)
-    if len(to_drop) != 0:
-        #         logger.info(f"Dropping non-buses {to_drop.tolist()}")
-        costs.drop(to_drop, level=0, inplace=True, axis=0, errors="ignore")
+    costs.drop(to_drop, level=0, inplace=True, axis=0, errors="ignore")
     # make sure they are removed from index
     costs.index = pd.MultiIndex.from_tuples(costs.index.values)
     return costs
 
 
-def get_link_widths(n, attr):
-    if attr == "total":
-        line_widths = n.lines.s_nom_opt
-        link_widths = n.links.p_nom_opt
-        title = "total grid"
-    else:
-        line_widths = n.lines.s_nom_opt - n.lines.s_nom_min
-        link_widths = n.links.p_nom_opt - n.links.p_nom_min
-        title = "added grid"
-
-    return line_widths, link_widths, title
-
-
-def plot_cost_map(
-    network: pypsa.Network,
-    opts: dict,
-    components=["generators", "links", "stores", "storage_units"],
-):
-    """plot a map of the costs
+def sum_components_costs(
+    ntwk: pypsa.Network,
+    components_list: list,
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """sum component costs
 
     Args:
-        network (pypsa.Network): _description_
-        opts (dict): _description_
-        components (list, optional): _description_. Defaults to ["generators", "links", "stores", "storage_units"].
+        ntwk (pypsa.Network): the network
+        components_list (list): the component type list
+
+    Returns:
+        tuple[pd.DataFrame, pd.DataFrame]: the total and pathway costs
     """
-    logger.info("Plotting cost map")
-    tech_colors = opts["tech_colors"]
 
-    n = network.copy()
-    assign_location(n)
     # Drop non-electric buses so they don't clutter the plot
-    n.buses.drop(n.buses.index[n.buses.carrier != "AC"], inplace=True)
+    costs_add = pd.DataFrame(index=ntwk.buses.index)
+    costs_nom = pd.DataFrame(index=ntwk.buses.index)
 
-    costs_add = pd.DataFrame(index=n.buses.index)
-    costs_nom = pd.DataFrame(index=n.buses.index)
-
-    for comp in components:
-        logger.info(f"getting {comp}")
-        df_c = getattr(n, comp)
+    for comp in components_list:
+        df_c = getattr(ntwk, comp)
 
         if df_c.empty:
             continue
 
         df_c["nice_group"] = df_c.carrier.map(rename_techs)
 
-        attr = "e_nom_opt" if comp == "stores" else "p_nom_opt"
-        attr2 = "e_nom" if comp == "stores" else "p_nom"
+        cap_name = "e_nom_opt" if comp == "stores" else "p_nom_opt"
+        cap_before_ext = "e_nom" if comp == "stores" else "p_nom"
 
-        costs_a = (
-            (df_c.capital_cost * (df_c[attr] - df_c[attr2]))
-            .groupby([df_c.location, df_c.nice_group])
-            .sum()
-            .unstack()
-            .fillna(0.0)
-        )
-        costs_add = pd.concat([costs_add, costs_a], axis=1)
+        costs_total = annualise_component_capex(df_c, cap_name)
+        costs_before_ext = annualise_component_capex(df_c, cap_before_ext)
+        costs_diff = costs_total - costs_before_ext
 
-        costs_n = (
-            (df_c.capital_cost * (df_c[attr]))
-            .groupby([df_c.location, df_c.nice_group])
-            .sum()
-            .unstack()
-            .fillna(0.0)
-        )
+        costs_add = pd.concat([costs_add, costs_diff], axis=1)
+        costs_nom = pd.concat([costs_nom, costs_total], axis=1)
 
-        costs_nom = pd.concat([costs_nom, costs_n], axis=1)
+    return costs_add, costs_nom
 
-    costs_add = get_costs(costs_add, tech_colors)
-    costs_nom = get_costs(costs_nom, tech_colors)
 
-    n.links.drop(
-        n.links.index[n.links.length == 0],
-        inplace=True,
-    )
+def add_cost_pannel(
+    df, fig: plt.Figure, preferred_order, tech_colors: dict, ax_loc=[-0.09, 0.28, 0.09, 0.45]
+) -> None:
+    """Add a cost pannel to the figure
 
-    threshold = 100e6  # 100 mEUR/a
-    carriers = pd.concat([costs_add, costs_nom]).groupby(level=1).sum()
-    carriers = carriers.where(carriers > threshold).dropna()
-    carriers = list(carriers.index)
+    Args:
+        df (_type_): _description_
+        fig (plt.Figure): the figure object to which the cost pannel will be added
+        preferred_order (_type_): index, the order in whiich to plot
+        tech_colors (dict): the tech colors
+        ax_loc (list, optional): _description_. Defaults to [-0.09, 0.28, 0.09, 0.45].
+    """
+    ax3 = fig.add_axes(ax_loc)
+    reordered = preferred_order.intersection(df.index).append(df.index.difference(preferred_order))
 
-    # PDF has minimum width, so set these to zero
-    line_lower_threshold = 500.0
-    line_upper_threshold = 1e4
-    linewidth_factor = opts["map"]["linewidth_factor"]
-    total_color = "rosybrown"
-    added_color = "darkseagreen"
-
-    df = pd.DataFrame(index=carriers, columns=["total", "added"])
-    df["total"] = costs_nom.groupby(level=1).sum()
-    df["added"] = costs_add.groupby(level=1).sum()
-    df = df.fillna(0)
-    df = df / 1e9
-    planning_horizon = int(snakemake.wildcards.planning_horizons)
-    df = df / (1 + snakemake.config["costs"]["discountrate"]) ** (planning_horizon - 2020)
-
-    new_index = preferred_order.intersection(df.index).append(df.index.difference(preferred_order))
-    percent = round((df.sum()[1] / df.sum()[0]) * 100)
-
-    fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw={"projection": ccrs.PlateCarree()})
-    fig.set_size_inches(opts["map"]["figsize"])
-
-    bus_size_factor = opts["map"]["cost_size_factor"]
-
-    line_widths, link_widths, title = get_link_widths(n, "total")
-
-    line_widths = line_widths.clip(line_lower_threshold, line_upper_threshold)
-    link_widths = link_widths.clip(line_lower_threshold, line_upper_threshold)
-
-    line_widths = line_widths.replace(line_lower_threshold, 0)
-    link_widths = link_widths.replace(line_lower_threshold, 0)
-
-    logger.info("Plotting total grid ax1")
-    n.plot(
-        bus_sizes=costs_nom / bus_size_factor,
-        bus_colors=tech_colors,
-        line_colors=total_color,
-        link_colors=total_color,
-        line_widths=line_widths / linewidth_factor,
-        link_widths=link_widths / linewidth_factor,
-        ax=ax1,
-        color_geomap=True,
-        boundaries=opts["map"]["boundaries"],
-    )
-
-    sizes = [10, 5]
-    labels = [f"{s} GW" for s in sizes]
-    scale = 1e3 / linewidth_factor
-    sizes = [s * scale for s in sizes]
-
-    legend_kw = dict(
-        loc="upper left",
-        bbox_to_anchor=(0.25, 1.03),
-        frameon=False,
-        labelspacing=0.8,
-        handletextpad=1,
-        title=title,
-    )
-
-    add_legend_lines(ax1, sizes, labels, patch_kw=dict(color=total_color), legend_kw=legend_kw)
-
-    line_widths, link_widths, title = get_link_widths(n, "added")
-
-    line_widths = line_widths.clip(line_lower_threshold, line_upper_threshold)
-    link_widths = link_widths.clip(line_lower_threshold, line_upper_threshold)
-
-    line_widths = line_widths.replace(line_lower_threshold, 0)
-    link_widths = link_widths.replace(line_lower_threshold, 0)
-    logger.info("Plotting added grid ax2")
-    n.plot(
-        bus_sizes=costs_add / bus_size_factor,
-        bus_colors=tech_colors,
-        line_colors=added_color,
-        link_colors=added_color,
-        line_widths=line_widths / linewidth_factor,
-        link_widths=link_widths / linewidth_factor,
-        ax=ax2,
-        color_geomap=True,
-        boundaries=opts["map"]["boundaries"],
-    )
-
-    sizes = [20, 10, 5]
-    labels = [f"{s} bEUR/a" for s in sizes]
-    sizes = [s / bus_size_factor * 1e9 for s in sizes]
-
-    legend_kw = dict(
-        loc="upper left",
-        bbox_to_anchor=(0.0, 1.03),
-        labelspacing=0.8,
-        frameon=False,
-        handletextpad=0,
-        title="system cost",
-    )
-    add_legend_circles(
-        ax1,
-        sizes,
-        labels,
-        srid=n.srid,
-        patch_kw=dict(facecolor="lightgrey"),
-        legend_kw=legend_kw,
-    )
-
-    add_legend_circles(
-        ax2,
-        sizes,
-        labels,
-        srid=n.srid,
-        patch_kw=dict(facecolor="lightgrey"),
-        legend_kw=legend_kw,
-    )
-
-    sizes = [10, 5]
-    labels = [f"{s} GW" for s in sizes]
-    scale = 1e3 / linewidth_factor
-    sizes = [s * scale for s in sizes]
-
-    legend_kw = dict(
-        loc="upper left",
-        bbox_to_anchor=(0.25, 1.03),
-        frameon=False,
-        labelspacing=0.8,
-        handletextpad=1,
-        title=title,
-    )
-
-    add_legend_lines(ax2, sizes, labels, patch_kw=dict(color=added_color), legend_kw=legend_kw)
-
-    legend_kw = dict(
-        bbox_to_anchor=(1.42, 1.04),
-        frameon=False,
-    )
-
-    colors = [tech_colors[c] for c in carriers] + [total_color, added_color]
-    labels = carriers + ["HVDC or HVAC link", "HVDC or HVAC link"]
-
-    add_legend_patches(
-        ax2,
-        colors,
-        labels,
-        legend_kw=legend_kw,
-    )
-
-    ax3 = fig.add_axes([-0.09, 0.28, 0.09, 0.45])
-
-    df.loc[new_index, df.columns].T.plot(
+    df.loc[reordered, df.columns].T.plot(
         kind="bar",
         ax=ax3,
         stacked=True,
-        color=[tech_colors[i] for i in new_index],
+        color=[tech_colors[i] for i in reordered],
     )
+    percent = round((df.sum()[1] / df.sum()[0]) * 100)
     ax3.legend().remove()
     ax3.set_ylabel("annualized system cost bEUR/a")
     ax3.set_xticklabels(ax3.get_xticklabels(), rotation="horizontal")
     ax3.grid(axis="y")
-    ax3.set_ylim([0, opts["costs_max"]])
+    ax3.set_ylim([0, df.sum().max() * 1.1])
+    # add label
     ax3.text(0.85, (df.sum()[1] + 15), str(percent) + "%", color="black")
+
+    fig.tight_layout()
+
+
+def plot_map(
+    network: pypsa.Network,
+    edge_widths: pd.Series,
+    bus_colors: pd.Series,
+    bus_sizes: pd.Series,
+    edge_colors: pd.Series | str = "black",
+    add_ref_edge_sizes=True,
+    add_ref_bus_sizes=True,
+    add_legend=True,
+    ax=None,
+    **kwargs,
+) -> plt.Axes:
+    """Plot the network on a map
+
+    Args:
+        network (pypsa.Network): the pypsa network (filtered to contain only relevant buses & links)
+        edge_colors (pd.Series|str): the series of edge colors
+        edge_widths (pd.Series): the edge widths
+        bus_colors (pd.Series): the series of bus colors
+        bus_sizes (pd.Series): the series of bus sizes
+        add_ref_edge_sizes (bool, optional): add reference line sizes in legend (requires edge_colors=True). Defaults to True.
+        add_ref_bus_sizes (bool, optional): add reference bus sizes in legend. Defaults to True.
+        ax (plt.Axes, optional): the plotting ax. Defaults to None (new figure).
+    """
+
+    if not ax:
+        fig, ax = plt.subplots()
+
+    network.plot(
+        bus_sizes=bus_sizes,
+        bus_colors=bus_colors,
+        line_colors=edge_colors,
+        link_colors=edge_colors,
+        line_widths=edge_widths,
+        link_widths=edge_widths,
+        ax=ax,
+        color_geomap=True,
+        boundaries=kwargs.get("boundaries", None),
+    )
+
+    if add_legend:
+        carriers = bus_sizes.index.get_level_values(1).unique().tolist()
+        colors = [tech_colors[c] for c in carriers]
+        if isinstance(edge_colors, str):
+            colors += [edge_colors]
+            labels = carriers + ["HVDC or HVAC link"]
+        else:
+            colors += edge_colors.values.to_list()
+            labels = carriers + edge_colors.index.to_list()
+        leg_opt = {"bbox_to_anchor": (1.42, 1.04), "frameon": False}
+        add_legend_patches(ax, colors, labels, legend_kw=leg_opt)
+
+    if add_ref_edge_sizes & (type(edge_colors) == str):
+        ref_unit = kwargs.get("ref_edge_unit", "GW")
+        size_factor = float(kwargs.get("linewidth_factor", 1e5))
+        ref_sizes = kwargs.get("ref_edge_sizes", [10e5, 5e5])
+        labels = [f"{float(s)/PLOT_CAP_UNITS} {ref_unit}" for s in ref_sizes]
+        ref_sizes = list(map(lambda x: float(x) / size_factor, ref_sizes))
+        legend_kw = dict(
+            loc="upper left",
+            bbox_to_anchor=(0.25, 1.0),
+            frameon=False,
+            labelspacing=0.8,
+            handletextpad=1,
+            title=kwargs.get("edge_ref_title", "Grid cap."),
+        )
+        add_legend_lines(
+            ax, ref_sizes, labels, patch_kw=dict(color=edge_colors), legend_kw=legend_kw
+        )
+
+    if add_ref_bus_sizes:
+        ref_unit = kwargs.get("ref_bus_unit", "bEUR/a")
+        size_factor = float(kwargs.get("bus_size_factor", 1e10))
+        ref_sizes = kwargs.get("ref_bus_sizes", [20e10, 10e10, 5e10])
+        labels = [f"{float(s)/PLOT_COST_UNITS} {ref_unit}" for s in ref_sizes]
+        ref_sizes = list(map(lambda x: float(x) / size_factor, ref_sizes))
+
+        legend_kw = {
+            "loc": "upper left",
+            "bbox_to_anchor": (0.0, 1.0),
+            "labelspacing": 0.8,
+            "frameon": False,
+            "handletextpad": 0,
+            "title": kwargs.get("bus_ref_title", "System cost"),
+        }
+
+        add_legend_circles(
+            ax,
+            ref_sizes,
+            labels,
+            srid=network.srid,
+            patch_kw=dict(facecolor="lightgrey"),
+            legend_kw=legend_kw,
+        )
+
+    return ax
+
+
+def plot_cost_map(
+    network: pypsa.Network,
+    opts: dict,
+    components=["generators", "links", "stores", "storage_units"],
+    base_year=2020,
+    cost_pannel=True,
+):
+    tech_colors = opts["tech_colors"]
+    plot_ntwk = network.copy()
+    plot_ntwk.buses.drop(plot_ntwk.buses.index[plot_ntwk.buses.carrier != "AC"], inplace=True)
+
+    assign_location(plot_ntwk)
+
+    plot_ntwk.links.drop(
+        plot_ntwk.links.index[plot_ntwk.links.length == 0],
+        inplace=True,
+    )
+
+    costs_pathway, costs_nom = sum_components_costs(plot_ntwk, components)
+    cost_pie = make_cost_pies(costs_pathway, tech_colors)
+    cost_pie_nom = make_cost_pies(costs_nom, tech_colors)
+
+    # TODO aggregate costs below threshold into "other" -> requires messing with network
+
+    # get all carrier types
+    carriers = (
+        cost_pie.index.get_level_values(1)
+        .unique()
+        .union(cost_pie_nom.index.get_level_values(1).unique())
+    )
+    carriers = carriers.tolist()
+
+    line_lower_threshold = 500.0
+    line_upper_threshold = 1e4
+
+    # Make figure with two pannels
+    fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw={"projection": ccrs.PlateCarree()})
+    fig.set_size_inches(opts["map"]["figsize"])
+
+    # Add the total costs
+    bus_size_factor = opts["map"]["cost_size_factor"]
+    linewidth_factor = opts["map"]["linewidth_factor"]
+    edge_widths = (
+        pd.concat([plot_ntwk.lines.s_nom_opt, plot_ntwk.links.p_nom_opt])
+        .clip(line_lower_threshold, line_upper_threshold)
+        .replace(line_lower_threshold, 0)
+    )
+    plot_map(
+        plot_ntwk,
+        edge_widths=edge_widths / linewidth_factor,
+        bus_colors=tech_colors,
+        bus_sizes=cost_pie_nom / bus_size_factor,
+        edge_colors="indigo",
+        ax=ax1,
+        add_legend=False,
+        **opts["cost_map"],
+    )
+
+    # Add the added costs
+    edge_widths_added = pd.concat(
+        [plot_ntwk.lines.s_nom_opt, plot_ntwk.links.p_nom_opt]
+    ) - pd.concat([plot_ntwk.lines.s_nom, plot_ntwk.links.p_nom])
+    plot_map(
+        plot_ntwk,
+        edge_widths=edge_widths_added / linewidth_factor,
+        bus_colors=tech_colors,
+        bus_sizes=cost_pie / bus_size_factor,
+        edge_colors="rosybrown",
+        ax=ax2,
+        add_legend=True,
+        **opts["cost_map"],
+    )
+
+    # Add the optional cost pannel
+    if cost_pannel:
+        df = pd.DataFrame(index=carriers, columns=["total", "added"])
+        df["total"] = cost_pie_nom.groupby(level=1).sum()
+        df["added"] = cost_pie.groupby(level=1).sum()
+        df = df.fillna(0)
+        df = df / PLOT_COST_UNITS
+        planning_horizon = int(snakemake.wildcards.planning_horizons)
+        df = df / (1 + snakemake.config["costs"]["discountrate"]) ** (planning_horizon - base_year)
+        add_cost_pannel(df, fig, preferred_order, tech_colors, ax_loc=[-0.09, 0.28, 0.09, 0.45])
 
     fig.tight_layout()
 
