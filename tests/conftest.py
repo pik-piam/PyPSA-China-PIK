55d2575f50a79f7394634a6fce33690a7b13de73
irr-github <ivanrafaelramirez@gmail.com>
2025-02-10 10:12:36 +0100

    add fixtures for integration tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,7 +28,7 @@ def load_config(config_path: PathLike) -> dict:
     return config_dict
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def make_snakemake_test_config(tmp_path_factory) -> dict:
     """make a test config for snamekemake based on the default config
     Example:
@@ -42,7 +42,7 @@ def make_snakemake_test_config(tmp_path_factory) -> dict:
     ) -> dict:
 
         base_config = load_config(DEFAULT_CONFIG)
-        base_config.update(load_config(TECH_CONFIG))
+        # base_config.update(load_config(TECH_CONFIG))
         base_config.update(kwargs)
 
         test_config = base_config.copy()
@@ -51,18 +51,44 @@ def make_snakemake_test_config(tmp_path_factory) -> dict:
         test_config["snapshots"]["frequency"] = time_res
         test_config["snapshots"]["start"] = start_d
         test_config["snapshots"]["end"] = end_d
+        # do not build rasters
+        test_config["enable"] = {k: False for k in test_config["enable"]}
 
         test_config["results_dir"] = str(tmp_path_factory.mktemp("results"))
         test_config["summary_dir"] = str(tmp_path_factory.mktemp("results_summary"))
         test_config["run"]["name"] = "automated_test_run"
+
+        test_config.pop("solving")
+        test_config.pop("atlite")
+
         return test_config
 
     return make
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
+def make_test_config_args(make_snakemake_test_config, request):
+    """expand dict for --config flag"""
+
+    # Get parameters passed via pytest.mark.parametrize
+    time_res = request.param.get("time_res", 24)
+    plan_year = request.param.get("plan_year", 2040)
+    kwargs = {k: v for k, v in request.param.items() if k not in ["time_res", "plan_year"]}
+    test_config = make_snakemake_test_config(time_res=time_res, plan_year=plan_year, **kwargs)
+
+    def flatten(x):
+        if type(x) is dict or type(x) is list:
+            return x
+        else:
+            return '["' + str(x) + '"]'
+
+    base_str = "REPTHIS".join([f"{k}={v}" for k, v in test_config.items()])
+    return r"'" + base_str.replace("'", '"').replace("REPTHIS", "' '") + r"'"
+
+
+@pytest.fixture(scope="module")
 def make_test_config_file(make_snakemake_test_config, tmpdir_factory, request):
-    """Fixture to save a temp config file for testing, return its path, and clean up after session."""
+    """Fixture to save a temp config file for testing, return its path, and clean up after module."""
 
     # Get parameters passed via pytest.mark.parametrize
     time_res = request.param.get("time_res", 24)
@@ -75,7 +101,7 @@ def make_test_config_file(make_snakemake_test_config, tmpdir_factory, request):
         hash_object = hash256(config_str.encode())
         return f"test_config_{hash_object.hexdigest()[:8]}.yaml"
 
-    # Create a temporary directory for the session
+    # Create a temporary directory for the module
     temp_dir = tmpdir_factory.mktemp("config_dir")
 
     # Generate the test config
